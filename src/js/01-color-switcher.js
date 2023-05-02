f5ac23f44e68b2f1e3e8d220c7eaccdfbb7a0cf4
VovaPliuto <vovaplyuto@gmail.com>
2023-05-02 13:04:11 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+let timeId = 0;
+
+const refs = {
+  startBtn: document.querySelector("[data-start]"),
+  stopBtn: document.querySelector("[data-stop]"),
+}
+
+refs.stopBtn.setAttribute('disabled', '');
+
+refs.startBtn.addEventListener("click", onStartBtnClick);
+refs.stopBtn.addEventListener("click", onStopBtnClick);
+
+function onStartBtnClick(e) {
+  refs.startBtn.setAttribute("disabled", "");
+  refs.stopBtn.removeAttribute("disabled");
+  timeId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick(e) {
+  clearInterval(timeId);
+   refs.stopBtn.setAttribute('disabled', '');
+   refs.startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
