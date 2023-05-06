1611d3d108cc459c441ba15ba0f46af30c64b023
VovaPliuto <vovaplyuto@gmail.com>
2023-05-06 16:12:43 +0300

    Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  formEl: document.querySelector('.form'),
+};
+let positionCounter = 1;
+
+refs.formEl.addEventListener('submit', onClickSubmit);
+
+function onClickSubmit(e) {
+  e.preventDefault();
+  let firstDelay = Number(refs.formEl.delay.value);
+  const step = Number(refs.formEl.step.value);
+  const amount = Number(refs.formEl.amount.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(positionCounter, firstDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    firstDelay += step;
+    positionCounter += 1;
+  }
+  positionCounter = 1;
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
